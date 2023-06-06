2823b93837830453ce854103b0c785b8df3669b2
Liubov Storozhenko <lybov5656@gmail.com>
2023-06-06 21:55:21 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp12/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsApp12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(23, 66);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(753, 315);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkGreen;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(316, 398);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Завантаження";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(295, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Інформація про ПК";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Lab19";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp12/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Management;
+
+namespace WindowsFormsApp12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            try
+            {
+                GetProcessorInfo();
+                GetVideoCardInfo();
+                GetDVDInfo();
+                GetDiskDriveInfo();
+                GetMotherboardInfo();
+                GetNetworkAdapterInfo();
+                GetBIOSInfo();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка при отриманні інформації: " + ex.Message, "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void GetProcessorInfo()
+        {
+            List<string> processorInfo = GetHardwareInfo("Win32_Processor", "Name");
+            OutputResult("Процесор:", processorInfo);
+            OutputResult("Виробник:", GetHardwareInfo("Win32_Processor", "Manufacturer"));
+            OutputResult("Опис:", GetHardwareInfo("Win32_Processor", "Description"));
+            richTextBox1.AppendText("\n");
+        }
+
+        private void GetVideoCardInfo()
+        {
+            OutputResult("Відеокарта:", GetHardwareInfo("Win32_VideoController", "Name"));
+            OutputResult("Відеопроцесор:", GetHardwareInfo("Win32_VideoController", "VideoProcessor"));
+            OutputResult("Версія драйверу:", GetHardwareInfo("Win32_VideoController", "DriverVersion"));
+            OutputResult("Об'єм пам'яті (в байтах):", GetHardwareInfo("Win32_VideoController", "AdapterRAM"));
+            richTextBox1.AppendText("\n");
+        }
+
+        private void GetDVDInfo()
+        {
+            OutputResult("Назва DVD:", GetHardwareInfo("Win32_CDROMDrive", "Name"));
+            OutputResult("Буква DVD:", GetHardwareInfo("Win32_CDROMDrive", "Drive"));
+            richTextBox1.AppendText("\n");
+        }
+
+        private void GetDiskDriveInfo()
+        {
+            OutputResult("Жорсткий диск:", GetHardwareInfo("Win32_DiskDrive", "Caption"));
+            OutputResult("Об'єм (в байтах):", GetHardwareInfo("Win32_DiskDrive", "Size"));
+            richTextBox1.AppendText("\n");
+        }
+
+        private void GetMotherboardInfo()
+        {
+            OutputResult("Материнська плата:", GetHardwareInfo("Win32_BaseBoard", "Product"));
+            richTextBox1.AppendText("\n");
+        }
+
+        private void GetNetworkAdapterInfo()
+        {
+            OutputResult("Мережевий адаптер:", GetHardwareInfo("Win32_NetworkAdapter", "Name"));
+            richTextBox1.AppendText("\n");
+        }
+
+        private void GetBIOSInfo()
+        {
+            OutputResult("BIOS:", GetHardwareInfo("Win32_BIOS", "Manufacturer"));
+            richTextBox1.AppendText("\n");
+        }
+
+        private List<string> GetHardwareInfo(string win32Class, string classItemField)
+        {
+            List<string> result = new List<string>();
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM " + win32Class);
+            try
+            {
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    result.Add(obj[classItemField].ToString().Trim());
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Помилка при отриманні інформації: " + ex.Message);
+            }
+            return result;
+        }
+
+        private void OutputResult(string info, List<string> result)
+        {
+            richTextBox1.AppendText(info + "\n");
+            for (int i = 0; i < result.Count; ++i)
+                richTextBox1.AppendText(result[i] + "\n");
+        }
+    }
+}
